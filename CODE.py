ec0838364197d3ea56267c08b249a90caf106a8b
Artigupta18 <110694314+Artigupta18@users.noreply.github.com>
2025-02-20 19:48:20 +0530

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -1,6 +1,7 @@
+import sqlite3
 __import__('pysqlite3')
 import sys
-sys.modules['sqlite3'] =sys.modules.pop('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 import google.generativeai as genai
